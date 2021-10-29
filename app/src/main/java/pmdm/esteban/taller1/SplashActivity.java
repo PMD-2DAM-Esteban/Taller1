cebfd8ad64093f2c0e4b9fc0e4eb1642836d8245
EstebanJPG <esteban.jimenez.piquerass@gmail.com>
2021-10-29 12:51:53 +0200

    Part Guiada Acabada

---
+++ app/src/main/java/pmdm/esteban/taller1/SplashActivity.java
@@ -0,0 +1,47 @@
+package pmdm.esteban.taller1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+private MediaPlayer sonido;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread time = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(1000);
+                    //play sonido
+
+                    sonido= MediaPlayer.create(getApplicationContext(), R.raw.pato);
+                    sonido.start();
+                    sleep(5000);
+
+
+                }catch (InterruptedException e){
+
+                }finally {
+                    startActivity(new Intent("pmdm.esteban.taller1.STARTINGPOINT"));
+                }
+            }
+        };
+
+        time.start();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sonido.release();
+        finish();
+    }
+}
